86a77ac70960495cc53d3dc54fb47400240a3f9b
Harris-Musah <hrrsabdallah123@gmail.com>
2025-03-11 13:30:52 +0000

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,107 +1,100 @@
-
 let humanChoice;
-let choices = document.getElementById('choices')
+let choices = document.getElementById("choices");
+let winner = document.getElementById("winner");
+let humanScore = 0;
+let computerScore = 0;
 
 const btn_1 = document.querySelector("#btn-1");
 const btn_2 = document.querySelector("#btn-2");
 const btn_3 = document.querySelector("#btn-3");
 
-
-
-function getComputerChoice(){
-    let CompChoice = Math.random()
-    if(CompChoice < 0.33){
-        return "ROCK"
-    }else if(CompChoice >0.33 && CompChoice < 0.66){
-        return "PAPER"
-    }else{
-        return "SCISSORS"
-    };
+function getComputerChoice() {
+  let CompChoice = Math.random();
+  if (CompChoice < 0.33) {
+    return "ROCK";
+  } else if (CompChoice >= 0.33 && CompChoice < 0.66) {
+    return "PAPER";
+  } else {
+    return "SCISSORS";
+  }
 }
 
-function getHumanChoice(humanChoice){
-    if (humanChoice == 1){
-        return "ROCK"
-    }else if(humanChoice == 2){
-        return "PAPER"
-    }else if (humanChoice == 3){
-        return "SCISSORS"
-    }else{
-        return "Start and choose between 1 and 3"
-    }
+function getHumanChoice(humanChoice) {
+  if (humanChoice == 1) {
+    return "ROCK";
+  } else if (humanChoice == 2) {
+    return "PAPER";
+  } else if (humanChoice == 3) {
+    return "SCISSORS";
+  } else {
+    return "Start and choose between 1 and 3";
+  }
 }
 
-function playRound(humanChoice, CompChoice){
-    if (humanChoice == CompChoice){
-        choices.innerHTML = "your choice: "+ humanChoice+", computer choice: "+ CompChoice;
-        return "it's a draw "
-    }else if(humanChoice == "ROCK" && CompChoice == "PAPER"){
-        choices.innerHTML = "your choice: "+ humanChoice+", computer choice: "+ CompChoice;
-        return "computer wins";
-    }else if(humanChoice == "PAPER" && CompChoice == "ROCK"){
-        choices.innerHTML = "your choice: "+ humanChoice+", computer choice: "+ CompChoice;
-        return "human wins";
-    }else if(humanChoice == "ROCK" && CompChoice == "SCISSORS"){
-        choices.innerHTML = "your choice: "+ humanChoice+", computer choice: "+ CompChoice;
-        return "human wins";
-    }else if(humanChoice == "SCISSORS" && CompChoice == "ROCK"){
-        choices.innerText = "your choice: "+ humanChoice+", computer choice: "+ CompChoice;
-        return "computer wins";
-    }else if(humanChoice == "SCISSORS" && CompChoice == "PAPER"){
-        choices.textContent = "your choice: "+ humanChoice+", computer choice: "+ CompChoice;
-        return "human wins";
-    }else if(humanChoice == "PAPER" && CompChoice == "SCISSORS"){
-        choices.innerHTML = "your choice: "+ humanChoice+", computer choice: "+ CompChoice;
-        return "computer wins";
-    };
+function playRound(humanChoice, CompChoice) {
+  if (humanChoice == CompChoice) {
+    choices.innerHTML =
+      "Your choice: " + humanChoice + ", Computer choice: " + CompChoice;
+    return "It's a draw";
+  } else if (humanChoice == "ROCK" && CompChoice == "PAPER") {
+    choices.innerHTML =
+      "Your choice: " + humanChoice + ", Computer choice: " + CompChoice;
+    return "Computer wins";
+  } else if (humanChoice == "PAPER" && CompChoice == "ROCK") {
+    choices.innerHTML =
+      "Your choice: " + humanChoice + ", Computer choice: " + CompChoice;
+    return "Human wins";
+  } else if (humanChoice == "ROCK" && CompChoice == "SCISSORS") {
+    choices.innerHTML =
+      "Your choice: " + humanChoice + ", Computer choice: " + CompChoice;
+    return "Human wins";
+  } else if (humanChoice == "SCISSORS" && CompChoice == "ROCK") {
+    choices.innerHTML =
+      "Your choice: " + humanChoice + ", Computer choice: " + CompChoice;
+    return "Computer wins";
+  } else if (humanChoice == "SCISSORS" && CompChoice == "PAPER") {
+    choices.innerHTML =
+      "Your choice: " + humanChoice + ", Computer choice: " + CompChoice;
+    return "Human wins";
+  } else if (humanChoice == "PAPER" && CompChoice == "SCISSORS") {
+    choices.innerHTML =
+      "Your choice: " + humanChoice + ", Computer choice: " + CompChoice;
+    return "Computer wins";
+  }
 }
 
-
-humanChoice = btn_1.addEventListener("click", () => {
-    humanChoice = 1;
-    console.log(humanChoice)
-    return humanChoice;
+btn_1.addEventListener("click", () => {
+  humanChoice = 1;
+  playGame();
 });
 
-humanChoice = btn_2.addEventListener("click", () => {
-    humanChoice = 2;
-    console.log(humanChoice)
-    return humanChoice;
+btn_2.addEventListener("click", () => {
+  humanChoice = 2;
+  playGame();
 });
 
-humanChoice = btn_3.addEventListener("click", () => {
-    humanChoice = 3;
-    console.log(humanChoice)
-    return humanChoice;
+btn_3.addEventListener("click", () => {
+  humanChoice = 3;
+  playGame();
 });
-// let humanChoice = prompt("Enter your choice 1.Rock , 2. Paper, 3. Scissors");
 
-    let humanScore = 0;
-    let computerScore = 0;
-        CompGuess = getComputerChoice();
-        humanGuess = getHumanChoice(humanChoice);        
-        play = playRound(humanGuess, CompGuess)
+function playGame() {
+  let CompGuess = getComputerChoice();
+  let humanGuess = getHumanChoice(humanChoice);
+  let result = playRound(humanGuess, CompGuess);
 
-        
+  if (result == "Computer wins") {
+    computerScore += 1;
+  } else if (result == "Human wins") {
+    humanScore += 1;
+  }
 
-        
+  winner.innerHTML =
+    "Human Score: " + humanScore + " Computer Score: " + computerScore;
 
-        // if(play == "computer wins"){
-        //     computerScore += 1;
-        // }else if (play == "human wins"){
-        //     humanScore += 1;
-        // }
-
-
-    // if(computerScore > humanScore){
-    //     console.log('computer wins')
-    // }else{
-    //     console.log('you win')
-    // }
-    // return 'computerScore:' + computerScore + ' humanscore:' + humanScore;
-
-
-// console.log(playGame());
-
-// console.log(CompGuess);
-// console.log(humanGuess);
+  if (computerScore > humanScore) {
+    console.log("Computer wins");
+  } else {
+    console.log("You win");
+  }
+}
